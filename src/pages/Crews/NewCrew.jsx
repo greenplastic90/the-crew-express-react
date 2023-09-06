f676d5a86e4d28cafc900e398b9b1255e59335b1
greenplastic90 <82140450+greenplastic90@users.noreply.github.com>
2023-09-06 16:50:10 +0300

    NewCrew can remove member names

--- src/pages/Crews/NewCrew.jsx
+++ src/pages/Crews/NewCrew.jsx
@@ -1,11 +1,8 @@
 import React, { useState } from 'react'
-import { Stack, Heading, FormLabel, Input, Button } from '@chakra-ui/react'
+import { Stack, Heading, FormLabel, Input, Button, HStack } from '@chakra-ui/react'
 
 function NewCrew() {
-	const [crew, setCrew] = useState({ name: '', memberNames: [''] })
-
-	const [error, setError] = useState('')
-	//! error if less than two and more than 5
+	const [crew, setCrew] = useState({ name: '', memberNames: ['', ''] })
 
 	function handleNameChange(e) {
 		setCrew({ ...crew, name: e.target.value })
@@ -22,28 +19,39 @@ function NewCrew() {
 		setCrew({ ...crew, memberNames: addedMemberName })
 	}
 
+	function deleteMember(index) {
+		const newMembersInput = [...crew.memberNames]
+		newMembersInput.splice(index, 1)
+		setCrew({ ...crew, memberNames: newMembersInput })
+	}
+
 	return (
-		<Stack>
-			<Heading as={'h1'} size={'2xl'}>
-				New Crew
-			</Heading>
-			<FormLabel>Name</FormLabel>
-			<Input type='text' name='name' value={crew.name} onChange={handleNameChange} required />
+		<form>
+			<Stack>
+				<Heading as={'h1'} size={'2xl'}>
+					New Crew
+				</Heading>
+				<FormLabel>Name</FormLabel>
+				<Input type='text' name='name' value={crew.name} onChange={handleNameChange} required />
 
-			<FormLabel>Members</FormLabel>
-			{crew.memberNames.map((member, index) => (
-				<Input
-					key={index}
-					type='text'
-					value={member}
-					onChange={(e) => handleMemberChange(e, index)}
-					placeholder={`Member ${index + 1}`}
-					required
-				/>
-			))}
+				<FormLabel>Members</FormLabel>
+				{crew.memberNames.map((member, index) => (
+					<HStack key={index}>
+						<Input
+							type='text'
+							value={member}
+							onChange={(e) => handleMemberChange(e, index)}
+							placeholder={`Member ${index + 1}`}
+							required
+						/>
+						{crew.memberNames.length > 2 && <Button onClick={() => deleteMember(index)}>X</Button>}
+					</HStack>
+				))}
 
-			<Button onClick={addMemberInput}>Add Member</Button>
-		</Stack>
+				{crew.memberNames.length < 5 && <Button onClick={addMemberInput}>Add Member</Button>}
+				<Button type='submit'>Done</Button>
+			</Stack>
+		</form>
 	)
 }
 
