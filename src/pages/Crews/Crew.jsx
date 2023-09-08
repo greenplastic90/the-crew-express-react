e2b801c1bc06f3cfc1c17c9d3d9e12550ebaa134
greenplastic90 <82140450+greenplastic90@users.noreply.github.com>
2023-09-08 13:37:10 +0300

    can filter missions by completed on Crew Page

--- src/pages/Crews/Crew.jsx
+++ src/pages/Crews/Crew.jsx
@@ -1,4 +1,4 @@
-import { HStack, Heading, Stack, Text } from '@chakra-ui/react'
+import { Checkbox, FormControl, FormLabel, HStack, Heading, Stack, Text } from '@chakra-ui/react'
 import React, { useEffect, useState } from 'react'
 import { getCrewById } from '../../utilities/crew-api'
 import { useParams } from 'react-router-dom'
@@ -7,9 +7,13 @@ import Missions from '../../components/Mission/Missions'
 function Crew() {
 	const [crew, setCrew] = useState(null)
 	const [missions, setMissions] = useState([])
+	const [filteredMissions, setFilteredMissions] = useState([])
+
+	const [showFiltered, setShowFiltered] = useState(false)
 
 	const { crewId } = useParams()
 
+	//* getCrew
 	useEffect(() => {
 		async function getCrew() {
 			try {
@@ -24,6 +28,18 @@ function Crew() {
 		}
 		getCrew()
 	}, [crewId])
+
+	//* Filter missions
+	useEffect(() => {
+		const filtered = missions.filter((mission) => {
+			if (!mission.tracker) {
+				return false
+			}
+			return !mission.tracker.completed
+		})
+		setFilteredMissions(filtered)
+	}, [missions, showFiltered])
+
 	return (
 		<>
 			{crew && (
@@ -31,13 +47,17 @@ function Crew() {
 					<Heading as={'h1'} size={'4xl'}>
 						{crew.name}
 					</Heading>
+					<FormControl>
+						<FormLabel>Hide Completed</FormLabel>
+						<Checkbox checked={showFiltered} onChange={() => setShowFiltered(!showFiltered)} />
+					</FormControl>
 					<HStack>
 						{crew.memberNames.map((name, i) => (
 							<Text key={i}>{name}</Text>
 						))}
 					</HStack>
 					<Text>Attempts: {crew.totalAttempts}</Text>
-					<Missions missions={missions} />
+					<Missions missions={showFiltered ? filteredMissions : missions} />
 				</Stack>
 			)}
 		</>
