b07cc989212798ebe6f075854285a3a57f643e23
greenplastic90 <82140450+greenplastic90@users.noreply.github.com>
2023-09-21 13:18:30 +0300

    PageWrapper Heading is not position is fixed - dynamically adjusts the hieght of children based on it

--- src/components/Miscellaneous/PageWrapper.jsx
+++ src/components/Miscellaneous/PageWrapper.jsx
@@ -1,13 +1,33 @@
-import { Heading, Stack } from '@chakra-ui/react'
-import React from 'react'
+import { Box, Heading, Stack } from '@chakra-ui/react'
+import React, { useRef, useEffect, useState } from 'react'
 
 function PageWrapper({ title, children }) {
+	const headingRef = useRef(null)
+	const [headingHeight, setHeadingHeight] = useState(0)
+
+	const updateHeadingHeight = () => {
+		console.log(headingRef.current.offsetHeight)
+		if (headingRef.current) {
+			setHeadingHeight(headingRef.current.offsetHeight)
+		}
+	}
+
+	useEffect(() => {
+		updateHeadingHeight()
+
+		window.addEventListener('resize', updateHeadingHeight)
+
+		return () => {
+			window.removeEventListener('resize', updateHeadingHeight)
+		}
+	}, [title])
+
 	return (
 		<Stack py={8} px={[null, 8, 16, 32, 64, 256]}>
-			<Heading variant={'h1'} as='h1' size='3xl' pb={5}>
+			<Heading ref={headingRef} position={'fixed'} variant={'h1'} as='h1' size='3xl' pb={5}>
 				{title.toUpperCase()}
 			</Heading>
-			{children}
+			<Box pt={headingHeight + 20}>{children}</Box>
 		</Stack>
 	)
 }
