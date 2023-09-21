6e6b1df53718ef01a8520c19194090178850c288
greenplastic90 <82140450+greenplastic90@users.noreply.github.com>
2023-09-21 14:23:29 +0300

    element card now has one color and a border

--- src/components/Miscellaneous/ElementCard.jsx
+++ src/components/Miscellaneous/ElementCard.jsx
@@ -3,7 +3,12 @@ import React from 'react'
 
 function ElementCard({ index = 2, children }) {
 	return (
-		<Stack p={4} bg={index % 2 === 0 ? 'brand.beigeLight' : 'brand.beige'} borderRadius={'sm'}>
+		<Stack
+			p={4}
+			bg={'brand.beige'}
+			borderRadius={'sm'}
+			border={'1em solid'}
+			borderColor={'brand.beigeLight'}>
 			{children}
 		</Stack>
 	)
