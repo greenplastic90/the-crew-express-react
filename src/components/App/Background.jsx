f7b18fe4d385a85ec3e825b32a334358028bf926
greenplastic90 <82140450+greenplastic90@users.noreply.github.com>
2023-09-25 13:45:11 +0300

    moved all background styling to Background comp

---
+++ src/components/App/Background.jsx
@@ -0,0 +1,30 @@
+import { Box } from '@chakra-ui/react'
+import React from 'react'
+import spaceBg from '../../images/space.jpg'
+
+function Background({ offset, children }) {
+	return (
+		<Box
+			position='relative'
+			display='grid'
+			gridTemplateRows='auto 1fr auto'
+			minHeight='100vh'
+			_before={{
+				content: '""',
+				position: 'absolute',
+				top: 0,
+				right: 0,
+				bottom: 0,
+				left: 0,
+				backgroundImage: `url(${spaceBg})`,
+				backgroundPosition: `${offset.x}px ${offset.y}px`,
+				filter: 'blur(2px)',
+				zIndex: -1,
+				transition: 'background-position 4s ease-in-out',
+			}}>
+			{children}
+		</Box>
+	)
+}
+
+export default Background
--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,34 +1,18 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import { Routes, Route, Navigate } from 'react-router-dom'
 import { getUser } from '../../utilities/users-service'
 import AuthPage from '../AuthPage/AuthPage'
 import NavBar from '../../components/NavBar/NavBar'
-import { Box, Button } from '@chakra-ui/react'
 import Crews from '../Crews/Crews'
 import NewCrew from '../Crews/NewCrew'
 import Crew from '../Crews/Crew'
 import UpdateCrew from '../Crews/UpdateCrew'
 import Mission from '../Missions/Mission'
-import spaceBg from '../../images/space.jpg'
+import Background from '../../components/App/Background'
 
 function App() {
 	const [user, setUser] = useState(getUser())
 	const [offset, setOffset] = useState({ x: 0, y: 0 })
-
-	// useEffect(() => {
-	// 	const handleScroll = () => {
-	// 		const newOffset = window.pageYOffset + 100
-	// 		setOffset({ x: newOffset, y: newOffset })
-	// 	}
-
-	// 	window.addEventListener('scroll', handleScroll)
-
-	// 	return () => window.removeEventListener('scroll', handleScroll)
-	// }, [])
-	// const handleScroll = () => {
-	// 	const newOffset = window.pageYOffset * 2
-	// 	setOffset({ x: newOffset, y: newOffset })
-	// }
 
 	const handleClick = () => {
 		const newOffset = offset.x + 500
@@ -36,26 +20,8 @@ function App() {
 	}
 
 	return (
-		<Box
-			position='relative'
-			display='grid'
-			gridTemplateRows='auto 1fr auto'
-			minHeight='100vh'
-			_before={{
-				content: '""',
-				position: 'absolute',
-				top: 0,
-				right: 0,
-				bottom: 0,
-				left: 0,
-				backgroundImage: `url(${spaceBg})`,
-				backgroundPosition: `${offset.x}px ${offset.y}px`,
-				filter: 'blur(2px)',
-				zIndex: -1,
-				transition: 'background-position 4s ease-in-out',
-			}}>
+		<Background offset={offset}>
 			<NavBar user={user} setUser={setUser} />
-			<Button onClick={handleClick}>Change Offset</Button>
 			{user ? (
 				<Routes>
 					<Route path='/' element={<Navigate to='/crews' />} />
@@ -68,7 +34,7 @@ function App() {
 			) : (
 				<AuthPage setUser={setUser} />
 			)}
-		</Box>
+		</Background>
 	)
 }
 
