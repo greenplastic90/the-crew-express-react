fbc85247cfe0e654aec6ac46c77f70a724907f31
greenplastic90 <82140450+greenplastic90@users.noreply.github.com>
2023-09-25 17:42:25 +0300

    MissionThumbNail Links to indivisual missions

--- src/components/Mission/MissionThumbNail.jsx
+++ src/components/Mission/MissionThumbNail.jsx
@@ -2,13 +2,23 @@ import { VStack } from '@chakra-ui/react'
 import React from 'react'
 import Pentagon from './Pentagon'
 import ElementCard from '../Miscellaneous/ElementCard'
+import { useNavigate } from 'react-router-dom'
+import { useBackgroundScroll } from '../Context/BackgroundScrollContext'
 
 function MissionThumbNail({ mission, tracker }) {
 	const { number, fivePlayerRule, tasks, taskTokens, description } = mission
-	const { attempts, distressSignalUsed, completed } = tracker
+	const { _id, attempts, distressSignalUsed, completed } = tracker
+
+	const navigate = useNavigate()
+	const { handleBackgroundScroll } = useBackgroundScroll()
+
+	function handleNavigateToMission() {
+		navigate(`/mission/${_id}`)
+		handleBackgroundScroll('north-east', 500)
+	}
 	return (
 		<ElementCard>
-			<VStack>
+			<VStack w={'100%'} h={'100%'} onClick={handleNavigateToMission} cursor={'pointer'}>
 				<Pentagon number={number} fivePlayerRule={fivePlayerRule} />
 			</VStack>
 		</ElementCard>
