0a15ab941672655656615768f02f06dd2f5a9084
greenplastic90 <82140450+greenplastic90@users.noreply.github.com>
2023-09-26 13:41:54 +0300

    cleaned up PageWrapper

--- src/components/Miscellaneous/PageWrapper.jsx
+++ src/components/Miscellaneous/PageWrapper.jsx
@@ -1,36 +1,9 @@
-import { Box, Heading, Stack } from '@chakra-ui/react'
-import React, { useRef, useEffect, useState } from 'react'
+import { Stack } from '@chakra-ui/react'
+import React from 'react'
 
 function PageWrapper({ title, children }) {
-	const headingRef = useRef(null)
-	const [headingHeight, setHeadingHeight] = useState(0)
-
-	const updateHeadingHeight = () => {
-		if (headingRef.current) {
-			setHeadingHeight(headingRef.current.offsetHeight)
-		}
-	}
-
-	useEffect(() => {
-		updateHeadingHeight()
-
-		window.addEventListener('resize', updateHeadingHeight)
-
-		return () => {
-			window.removeEventListener('resize', updateHeadingHeight)
-		}
-	}, [title])
-
 	return (
 		<Stack h={'100%'} pt={16} pb={8} px={[4, 8, 16, 32, 64, 256]}>
-			{/* {title && (
-				<Box ref={headingRef}>
-					<Heading variant={'h1'} as='h1' size='3xl' pb={5}>
-						{title.toUpperCase()}
-					</Heading>
-				</Box>
-			)} */}
-
 			{children}
 		</Stack>
 	)
