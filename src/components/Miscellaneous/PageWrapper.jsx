c16504f8c3264da0848408b068f385e5ac81a82b
greenplastic90 <82140450+greenplastic90@users.noreply.github.com>
2023-09-26 13:54:29 +0300

    removed title

--- src/components/Miscellaneous/PageWrapper.jsx
+++ src/components/Miscellaneous/PageWrapper.jsx
@@ -1,7 +1,7 @@
 import { Stack } from '@chakra-ui/react'
 import React from 'react'
 
-function PageWrapper({ title, children }) {
+function PageWrapper({ children }) {
 	return (
 		<Stack h={'100%'} pt={16} pb={8} px={[4, 8, 16, 32, 64, 256]}>
 			{children}
