82017638aa98d39d4e8f4f275384528f52cd7163
greenplastic90 <82140450+greenplastic90@users.noreply.github.com>
2023-10-09 17:11:49 +0300

    created adventure-api.js

---
+++ src/utilities/adventure-api.js
@@ -0,0 +1,26 @@
+import sendRequest from './send-request'
+const BASE_URL = '/api/adventures'
+
+export function getAllAdventures() {
+	return sendRequest(BASE_URL)
+}
+
+export function getAllAdventuresForAUser() {
+	return sendRequest(`${BASE_URL}/user`)
+}
+
+export function getAdventureById(id) {
+	return sendRequest(`${BASE_URL}/${id}`)
+}
+
+export function createAdventure(adventure) {
+	return sendRequest(BASE_URL, 'POST', adventure)
+}
+
+export function updateAdventure(adventure, adventureId) {
+	return sendRequest(`${BASE_URL}/${adventureId}`, 'PUT', adventure)
+}
+
+export function deleteAdventureById(id) {
+	return sendRequest(`${BASE_URL}/${id}`, 'DELETE')
+}
