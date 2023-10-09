ae4ac2a27dd316c92a329aedd7c49c2b01beb7d0
greenplastic90 <82140450+greenplastic90@users.noreply.github.com>
2023-10-09 13:09:24 +0300

    Created adventures controller

---
+++ controllers/api/adventures.js
@@ -0,0 +1,76 @@
+const Adventure = require('../../models/adventure')
+
+// Create a new Adventure
+const createAdventure = async (req, res) => {
+	try {
+		const adventure = new Adventure({ ...req.body, owner: req.user._id })
+		await adventure.save()
+		res.status(201).json(adventure)
+	} catch (error) {
+		res.status(400).json({ error: error.message })
+	}
+}
+
+// Get all Adventures
+const getAllAdventures = async (req, res) => {
+	try {
+		const adventures = await Adventure.find({ owner: req.user._id })
+		res.status(200).json(adventures)
+	} catch (error) {
+		res.status(400).json({ error: error.message })
+	}
+}
+
+// Get a single Adventure by ID
+const getAdventureById = async (req, res) => {
+	try {
+		const { id } = req.params
+		const adventure = await Adventure.findOne({ _id: id, owner: req.user._id })
+		if (!adventure) {
+			return res.status(404).json({ error: 'Adventure not found' })
+		}
+		res.status(200).json(adventure)
+	} catch (error) {
+		res.status(400).json({ error: error.message })
+	}
+}
+
+// Update an Adventure by ID
+const updateAdventureById = async (req, res) => {
+	try {
+		const { id } = req.params
+		const adventure = await Adventure.findByIdAndUpdate(
+			{ _id: id, owner: req.user._id },
+			req.body,
+			{ new: true, runValidators: true }
+		)
+		if (!adventure) {
+			return res.status(404).json({ error: 'Adventure not found' })
+		}
+		res.status(200).json(adventure)
+	} catch (error) {
+		res.status(400).json({ error: error.message })
+	}
+}
+
+// Delete an Adventure by ID
+const deleteAdventureById = async (req, res) => {
+	try {
+		const { id } = req.params
+		const adventure = await Adventure.findOneAndRemove({ _id: id, owner: req.user._id })
+		if (!adventure) {
+			return res.status(404).json({ error: 'Adventure not found' })
+		}
+		res.status(200).json({ message: 'Adventure deleted successfully' })
+	} catch (error) {
+		res.status(400).json({ error: error.message })
+	}
+}
+
+module.exports = {
+	createAdventure,
+	getAllAdventures,
+	getAdventureById,
+	updateAdventureById,
+	deleteAdventureById,
+}
