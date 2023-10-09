09b1081d42053d6942a303a30f2f1de3d08cf193
greenplastic90 <82140450+greenplastic90@users.noreply.github.com>
2023-10-09 14:05:07 +0300

    created adventure routes

---
+++ routes/api/adventures.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const router = express.Router()
+const adventureCtrl = require('../../controllers/api/adventures')
+
+router.post('/', adventureCtrl.createAdventure)
+router.get('/', adventureCtrl.getAllAdventures)
+router.get('/:id', adventureCtrl.getAdventureById)
+router.put('/:id', adventureCtrl.updateAdventureById)
+router.delete('/:id', adventureCtrl.deleteAdventureById)
+
+module.exports = router
