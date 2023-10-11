3746bac3a72eede24c29e47b4e34d19c5b6c5f7c
greenplastic90 <82140450+greenplastic90@users.noreply.github.com>
2023-10-11 14:23:23 +0300

    Adventure - create Adventure button added

--- src/pages/Adventures/Adventures.jsx
+++ src/pages/Adventures/Adventures.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 
 import AdventuresDisplay from '../../components/Adventure/AdventuresDisplay'
+import { Button, Stack } from '@chakra-ui/react'
+import { useNavigation } from '../../components/Context/NavigationContext'
 
 function Adventures() {
-	return <AdventuresDisplay />
+	const { handleNavigation } = useNavigation()
+	return (
+		<Stack>
+			<Button onClick={() => handleNavigation('/adventures/new', 'north')}>Create Adventure</Button>
+			<AdventuresDisplay />
+		</Stack>
+	)
 }
 
 export default Adventures
