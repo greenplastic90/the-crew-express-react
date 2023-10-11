626a86904b33e29db8172d75edbe3591b2247a97
greenplastic90 <82140450+greenplastic90@users.noreply.github.com>
2023-10-11 18:02:52 +0300

    UpdateDeleteButtons added to AdventureDetails

--- src/components/Adventure/AdventureDetails.jsx
+++ src/components/Adventure/AdventureDetails.jsx
@@ -1,14 +1,16 @@
 import { Button, HStack, Heading, Stack, Text } from '@chakra-ui/react'
 import React from 'react'
 import ElementCard from '../Miscellaneous/ElementCard'
-import { MdDeleteForever, MdEditSquare } from 'react-icons/md'
 import { useNavigation } from '../Context/NavigationContext'
 import { parseBoldText } from '../../utilities/miscellaneous'
+import UpdateDeleteButtons from '../Miscellaneous/UpdateDeleteButtons'
+import { getUser } from '../../utilities/users-service'
 
 function AdventureDetails({ adventure, showPreviewButton = false }) {
 	const { _id, name, description, owner, official } = adventure
-
 	const { handleNavigation } = useNavigation()
+
+	const isOwner = owner._id === getUser()._id
 
 	return (
 		<ElementCard>
@@ -16,32 +18,37 @@ function AdventureDetails({ adventure, showPreviewButton = false }) {
 				<Heading variant={'adventure'}>{name}</Heading>
 				{!official && (
 					<HStack>
-						<Text>Author :</Text>
+						<Text>Author:</Text>
 						<Text color={'brand.brown.500'}>{owner.username}</Text>
 					</HStack>
 				)}
-				<HStack>
-					<Button variant={'default'} onClick={() => handleNavigation(`/adventures/${_id}/edit`)}>
-						<MdEditSquare size={20} />
-					</Button>
-				</HStack>
+
 				<Text variant={'description'}>{parseBoldText(description)}</Text>
-				<HStack>
-					{showPreviewButton && (
-						<Button
-							variant={'default'}
-							w={'100%'}
-							onClick={() => handleNavigation(`/adventures/${_id}/preview`, 'south')}>
-							Preview
-						</Button>
-					)}
+				<Stack>
+					<HStack>
+						{showPreviewButton && (
+							<Button
+								variant={'default'}
+								w={'100%'}
+								onClick={() => handleNavigation(`/adventures/${_id}/preview`, 'south')}>
+								Preview
+							</Button>
+						)}
+						{isOwner && (
+							<UpdateDeleteButtons
+								name={name}
+								updateFormPath={`/adventures/${_id}/edit`}
+								deleteFunc={() => console.log('Not implemented yet')}
+							/>
+						)}
+					</HStack>
 					<Button
 						variant={'advance'}
 						w={'100%'}
 						onClick={() => handleNavigation('/crews/new', 'north', { adventureId: _id })}>
 						Create Crew
 					</Button>
-				</HStack>
+				</Stack>
 			</Stack>
 		</ElementCard>
 	)
