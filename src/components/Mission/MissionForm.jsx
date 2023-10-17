9f7eb2592640906f974df85288dd31fb30a71dbe
greenplastic90 <82140450+greenplastic90@users.noreply.github.com>
2023-10-17 13:33:12 +0300

    added Number of Tasks feild

--- src/components/Mission/MissionForm.jsx
+++ src/components/Mission/MissionForm.jsx
@@ -1,16 +1,24 @@
 import React from 'react'
 import ElementCard from '../Miscellaneous/ElementCard'
 import FormWrapper from '../Miscellaneous/FormWrapper'
-import { FormLabel, NumberInput, NumberInputField } from '@chakra-ui/react'
+import { FormControl, FormLabel, NumberInput, NumberInputField } from '@chakra-ui/react'
 
 function MissionForm({ mission }) {
 	return (
 		<ElementCard>
 			<FormWrapper>
-				<FormLabel>Mission Number</FormLabel>
-				<NumberInput>
-					<NumberInputField />
-				</NumberInput>
+				<FormControl>
+					<FormLabel>Mission Number</FormLabel>
+					<NumberInput>
+						<NumberInputField />
+					</NumberInput>
+				</FormControl>
+				<FormControl>
+					<FormLabel>Number of Tasks</FormLabel>
+					<NumberInput>
+						<NumberInputField />
+					</NumberInput>
+				</FormControl>
 			</FormWrapper>
 		</ElementCard>
 	)
